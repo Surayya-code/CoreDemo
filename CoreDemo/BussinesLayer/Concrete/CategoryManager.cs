24179fba3bf9b79cc8c0d23dec938c50935f90bc
surayya-code <ACER@DESKTOP-DTQOSA4>
2021-11-21 14:09:23 +0400

    add CoreDemo

---
+++ CoreDemo/BussinesLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,44 @@
+﻿using BussinesLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository efCategoryRepository;
+        public CategoryManager()
+        {
+            efCategoryRepository = new EfCategoryRepository();
+        }
+        public void CategoryAdd(Category category)
+        {
+            efCategoryRepository.Insert(category);
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            efCategoryRepository.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            efCategoryRepository.Update(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return efCategoryRepository.GetById(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return efCategoryRepository.GetListAll();
+        }
+    }
+}
